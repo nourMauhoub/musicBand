e463c51c501c18dce2279e1151f61bcaab0c5c37
nour elmauhoub <nelamauhoub@gmail.com>
2023-02-08 02:49:17 -0800

    first commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern':  "url('../assets/crew3.jpg')",
+        },
+        colors: {
+            'regal-blue': '#1d2671cc',
+            'red':'#c33764cc',
+          },
+      }
+    }
+  }
